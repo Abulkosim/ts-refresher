98e8fc8dc0e206ea2f74040fed3941437ac6d620
Abulkosim <abulkasimkattaev@gmail.com>
2025-06-01 12:33:29 +0500

    remove: js versions

--- classes.js
+++
@@ -1,8 +0,0 @@
-var Point = /** @class */ (function () {
-    function Point() {
-    }
-    return Point;
-}());
-var pt = new Point();
-pt.x = 0;
-pt.y = 0;
--- problem_01.js
+++
@@ -1,8 +0,0 @@
-function formatUserDetails(user) {
-    return "".concat(user.name, " ").concat(user.age, " ").concat(user.address);
-}
-console.log(formatUserDetails({
-    name: 'Abulkosim',
-    age: 22,
-    address: 'Tashkent'
-}));
