1a24c50e98258ca3ff60221db096203d8d4019d9
Abulkosim <abulkasimkattaev@gmail.com>
2025-09-09 00:05:32 +0500

    promise all - not my solution

--- type_challenges/promiseAll.ts
+++ type_challenges/promiseAll.ts
@@ -4,5 +4,8 @@ const promise3 = new Promise<string>((resolve, reject) => {
   setTimeout(resolve, 100, 'foo');
 });
 
+declare function PromiseAll<T extends any[]>(values: readonly [...T]):
+  Promise<{ [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K] }>;
+  
 // expected to be `Promise<[number, 42, string]>`
-const p = PromiseAll([promise1, promise2, promise3] as const)
+const p = PromiseAll([promise1, promise2, promise3] as const) 
